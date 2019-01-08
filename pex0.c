54fc528e18f4095c1ee2b772e64081ff2e9ddbe3
triciadang <c20tricia.dang@usafa.edu>
2019-01-08 07:09:50 -0800

    just need to do structure on part g

--- pex0.c
+++ pex0.c
@@ -1,14 +1,38 @@
 /** PEX0.c
  * =============================================================
- * Name:
- * Section:
+ * Name: Tricia Dang
+ * Section: M3
  * Project: PEX0 - C Proficiency
  * Documentation Statement:
  * =============================================================
  */
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
     printf("PEX0\n");
+
+    printf(argv[1]);
+    printf("\n");
+
+    for (int i = 1; i<21; i++){
+        if (i%3 == 0){
+            printf("%d\n",i);
+        }
+    }
+
+    int* intArray = {5,4,3,2,1};
+    printf("%d\n", func1(intArray));
+
+    NodeType node1;
+    node1.
+    strncpy("", node1)
+
+
+
     return 0;
+}
+
+int func1(int* intArray){
+    return intArray[0]*2;
 }
---
+++ pex0.h
@@ -0,0 +1,17 @@
+//
+// Created by triciadang on 1/8/19.
+//
+
+#ifndef PEX0_PEX0_H
+#define PEX0_PEX0_H
+
+int func1Array(int*);
+
+typdef struct node{
+    char[64] name;
+    node* next;
+}node;
+
+typedef node* Nodetype;
+
+#endif //PEX0_PEX0_H
